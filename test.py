a825183e11db68213b62552facad73d82018ba66
beleabhijit21 <103210770+beleabhijit21@users.noreply.github.com>
2023-12-27 16:36:26 +0530

    Update test.py
    chatgpt output

--- test.py
+++ test.py
@@ -5,8 +5,10 @@ from app import app
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
+
+    with app.app_context():
+        with app.test_client() as client:
+            yield client
 
 
 def test_app_is_working(client):
